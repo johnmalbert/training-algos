c48243622fc10183e26954d6b150aedcf4ae9337
John Albert <john.m.albert@gmail.com>
2021-04-20 08:45:16 -0700

    added findDigits in C#

---
+++ find-digits/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+An integer d is a divisor of an integer n if the remainder of d % n == 0.
+
+Given an integer, for each digit that makes up the integer determine whether it is a divisor. Count the number of divisors occurring within the integer.
+
+Example: 1024 will return 3, the digits 1, 2, and 4 are all divisors of 1024. 0 is not.
+
+*/
+
+using System;
+
+namespace find_digits
+{
+    class Program
+    {
+        static int findDigits(int n) {
+        string s = n.ToString();
+        int count = 0;
+        for(int i = 0; i < s.Length; i++)
+        {
+            string place = s[i].ToString();
+            int current = Int32.Parse(place);
+            if(current != 0)
+            {
+                if(n % current == 0)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+
+    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(findDigits(1012));
+            Console.WriteLine(findDigits(123456));
+            Console.WriteLine(findDigits(13));
+        }
+    }
+}
---
+++ powInPython.py
@@ -0,0 +1,28 @@
+# You are given three integers: a, b, and m. Print two lines.
+# On the first line, print the result of pow(a,b). On the second line, print the result of pow(a,b,m).
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a,b))
+print(pow(a,b,m))
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Task
+# Read four numbers, and print the result of a^b + c^d
+
+# Input Format
+# Integers are given on four separate lines, respectively.
+import math
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(pow(a,b) + pow(c,d))
+
